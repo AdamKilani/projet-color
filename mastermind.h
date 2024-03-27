0689203a541b92513679b7d9c6d4b0ffbe4d98fc
AdamKilani <142688635+AdamKilani@users.noreply.github.com>
2024-03-27 12:56:20 +0100

    Update mastermind.h

--- mastermind.h
+++ mastermind.h
@@ -1,1 +1,21 @@
+#ifndef MASTERMIND_H
+#define MASTERMIND_H
+
+#include "color_library.h"
+
+#define CODE_LENGTH 4
+#define MAX_GUESSES 10
+
+typedef enum {
+    Red, Cyan, Yellow, Green, Blue, Purple
+} Color;
+
+void generateSecretCode(Color secretCode[]);
+void printInstructions();
+void printColors(Color colors[]);
+void evaluateGuess(Color secretCode[], Color guess[], int guessNum);
+int countCorrect(Color secretCode[], Color guess[]);
+int countMisplaced(Color secretCode[], Color guess[]);
+
+#endif // MASTERMIND_H
 
