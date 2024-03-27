e899177a832367129c8875f0fda4d1607b29bd3a
AdamKilani <142688635+AdamKilani@users.noreply.github.com>
2024-03-27 13:04:43 +0100

    improve

--- file_handling.c
+++ file_handling.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int loadSecretCodeFromFile(const char *filename, Color secretCode[]) {
+static void print_error(const char *filename) {
+    printf("Error: Could not open file %s\n", filename);
+}
+
+int load_secret_code_from_file(const char *filename, Color secret_code[]) {
     FILE *file = fopen(filename, "r");
     if (!file) {
-        printf("Error: Could not open file %s\n", filename);
+        print_error(filename);
         return 0;
     }
 
@@ -13,21 +17,21 @@ int loadSecretCodeFromFile(const char *filename, Color secretCode[]) {
     int index = 0;
     while (fscanf(file, "%d", &color) == 1) {
         if (index >= CODE_LENGTH) {
-            printf("Error: Invalid color sequence in file %s (too long)\n", filename);
+            print_error(filename);
             fclose(file);
             return 0;
         }
-        secretCode[index++] = color;
+        secret_code[index++] = color;
     }
 
     fclose(file);
 
     if (index != CODE_LENGTH) {
-        printf("Error: Invalid color sequence in file %s (too short)\n", filename);
+        print_error(filename);
         return 0;
     }
 
-    if (!isValidColorSequence(secretCode)) {
+    if (!is_valid_color_sequence(secret_code)) {
         printf("Error: Invalid color sequence in file %s (invalid colors)\n", filename);
         return 0;
     }
@@ -35,7 +39,7 @@ int loadSecretCodeFromFile(const char *filename, Color secretCode[]) {
     return 1;
 }
 
-int isValidColorSequence(Color sequence[]) {
+int is_valid_color_sequence(Color sequence[]) {
     for (int i = 0; i < CODE_LENGTH; i++) {
         if (sequence[i] < Red || sequence[i] > Purple) {
             return 0;
