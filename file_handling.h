f4fccc88fedb13980e852f69c50d8c7d1434c11a
AdamKilani <142688635+AdamKilani@users.noreply.github.com>
2024-03-27 13:04:43 +0100

    improve

--- file_handling.h
+++ file_handling.h
@@ -1,9 +1,54 @@
 #ifndef FILE_HANDLING_H
 #define FILE_HANDLING_H
 
+#include <stdbool.h>
 #include "mastermind.h"
 
 int loadSecretCodeFromFile(const char *filename, Color secretCode[]);
-int isValidColorSequence(Color sequence[]);
+bool isValidColorSequence(Color sequence[], int length);
 
 #endif // FILE_HANDLING_H
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "file_handling.h"
+#include "mastermind.h"
+
+int loadSecretCodeFromFile(const char *filename, Color secretCode[])
+{
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        return -1; // error opening file
+    }
+
+    int count = 0;
+    while (fscanf(file, "%c", &secretCode[count]) == 1) {
+        if (!isValidColor(secretCode[count])) {
+            fclose(file);
+            return -2; // error: invalid color in file
+        }
+        count++;
+        if (count >= MAX_SECRET_CODE_LENGTH) {
+            break;
+        }
+    }
+    fclose(file);
+
+    if (count < MAX_SECRET_CODE_LENGTH) {
+        return -3; // error: file does not contain enough colors
+    }
+
+    return 0; // success
+}
+
+bool isValidColorSequence(Color sequence[], int length)
+{
+    for (int i = 0; i < length; i++) {
+        if (!isValidColor(sequence[i])) {
+            return false;
+        }
+    }
+    return true;
+}
