11adf8beb5a838ef989ab70809cf9e3183edcec7
AdamKilani <142688635+AdamKilani@users.noreply.github.com>
2024-03-28 18:09:55 +0100

    Update color_library.c

--- color_library.c
+++ color_library.c
@@ -1,25 +1,8 @@
-#include "color_library.h"
-#include <stdio.h>
+// color_library.c
 
-void print(Color color) {
-    switch (color) {
-        case Red:
-            printf("\033[0;31mRed\033[0m");
-            break;
-        case Cyan:
-            printf("\033[0;36mCyan\033[0m");
-            break;
-        case Yellow:
-            printf("\033[0;33mYellow\033[0m");
-            break;
-        case Green:
-            printf("\033[0;32mGreen\033[0m");
-            break;
-        case Blue:
-            printf("\033[0;34mBlue\033[0m");
-            break;
-        case Purple:
-            printf("\033[0;35mPurple\033[0m");
-            break;
-    }
+#include <stdio.h>
+#include "color_library.h"
+
+void print_in_color(char text[], char color[]) {
+    printf("%s%s%s", color, text, RESET);
 }
